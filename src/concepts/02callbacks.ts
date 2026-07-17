57c75a24634d15985bd0c502e60b72989df305e6
seiku2000 <eslumter@gmail.com>
2026-07-17 14:18:00 -0700

    callback

--- src/concepts/02callbacks.ts
+++ src/concepts/02callbacks.ts
@@ -5,15 +5,29 @@ export const CallbackComponent = (element: HTMLDivElement) => {
     let id= "5d86371f1efebc31def272e2";
   //  console.log('CallbackComponent');
 
+  //implemantado callback
+    findHero(id, (errorMessage,IroMan) =>{
+        if(errorMessage){
+            element.innerHTML = errorMessage;
+            return
+        }
 
-    findHero(id, (IroMan) =>{
-        console.log(IroMan);
-        element.innerHTML = IroMan?.name;
+       // console.log(IroMan);
+        element.innerHTML = IroMan.name;
+        
+       // element.innerHTML = IroMan?.name || "hero no encontrado";
     })
 }
-
+//callback implementation
 const findHero:callBackInterface = (id:string,callback)=>{
+  
     const heroe =heroes.find(heroe => heroe.id === id);
-    callback(heroe)
+    
+    if(!heroe){
+        callback(`heroe no encontrad con el id${id}`)
+        return
+    }
+    callback(null, heroe)
 
 }
+
